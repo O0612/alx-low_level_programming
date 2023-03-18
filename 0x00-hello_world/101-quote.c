3badf72b62168bb6a305b5b0fda054f06bf42fd7
o0612 <okezie.nkemakolam@routelinksys.com>
2023-03-18 03:31:19 -0700

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       write(C2,
+       "add that piece of art work is useful\" - Dora Korpar, 2015-1-19\n" 59;
+       return (1)
+}
+
