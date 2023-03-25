8415410b641292269e8c5e8a74896796a6e9944c
O0612 <okezie.nkemakolam@routelinksys.com>
2023-03-25 03:27:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual
+ * puzzle,followed by a newline."
+ * return 0.
  */
+
 int main(void)
+
 {
-       puts("\"programming is like building a multiligual puzzle");
-       return(0);
+
+       puts("\"Programming is like building a multilingual");
+
+       return (0);
 }
